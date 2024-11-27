2984eebd5fd892dc9179b24fabc08bf28eb7ed36
nphuong06 <nguyenphuong2006123@gmail.com>
2024-11-27 07:31:33 +0700

    Add files via upload

---
+++ Session06-B3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main (){
+	int n;
+	while (1 ){
+		printf("MOi ban nhap mat khau : "); 
+		scanf("%d",&n);
+		if (n == 8386){
+			printf("dung roi\n ");
+			break;
+		}
+		else {
+			printf("moi ban nhap lai \n"); 
+		}  
+	}
+	
+	
+	return 0; 
+}
